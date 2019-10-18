b5714fbfa5c917604e8f84c61eff1de159b15dc4
Steve Smith <steve@kentsmiths.com>
2019-10-18 12:20:12 -0400

    Initial projects created

---
+++ src/CleanArchitecture.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchitecture.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/CleanArchitecture.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
