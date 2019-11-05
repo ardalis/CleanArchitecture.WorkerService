dfbc8272b4ba35d5d0dc7f3fd3140886583dd1ca
Kevin Logan <kevin.logan@chrsolutions.com>
2019-11-05 15:59:17 -0600

    fix interface

--- src/CleanArchitecture.Core/Services/IServiceLocator.cs
+++ src/CleanArchitecture.Core/Services/IServiceLocator.cs
@@ -6,7 +6,6 @@ namespace CleanArchitecture.Core.Services
     public interface IServiceLocator : IDisposable
     {
         IServiceScope CreateScope();
-        void Dispose();
         T Get<T>();
     }
 }
